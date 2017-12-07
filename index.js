c9ee872f46825659fc0d1c330753a0422568c890
ido <ido@dooble.co.il>
2017-12-08 00:01:29 +0200

    changed name to "core.import.prop-types"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ PropTypes.function = PropTypes.func;
 PropTypes.boolean = PropTypes.bool;
 
 module.exports = {
-    name: 'core.imports.prop-types',
+    name: 'core.import.prop-types',
     imports: {
         'prop-types': PropTypes,
         PropTypes: PropTypes
